bd0a106656c8e4cc51e91dc2cd84726470e43047
FrYt4 <adrian.fryczkie@yahoo.com>
2024-02-09 17:28:34 +0100

    hw01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,29 +1,27 @@
-const  bodyChangeColor = document.querySelector(`body`);
+const bodyChangeColor = document.querySelector(`body`);
 const btnStart = document.querySelector(`[data-start]`);
 const btnStop = document.querySelector(`[data-stop]`);
 
-let intervalid = null
+let intervalId = null;
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
 
+btnStop.setAttribute("disabled", "");
 
-  btnStop.setAttribute("disabled", "");
+btnStart.addEventListener('click', element => {
+    btnStart.setAttribute("disabled", true);
+    btnStop.removeAttribute("disabled");
 
-  btnStart.addEventListener(`click`, element =>{
-  element.target.setAttribute("disabled" , true );
-  btnStart.removeAttribute("disabled");
-
-    setInterval.setInterval(() =>{
+    intervalId = setInterval(() => {
         bodyChangeColor.style.backgroundColor = getRandomHexColor();
-    },1000);
-  });
+    }, 1000);
+});
 
-
-    btnStop.addEventListener('click', element => {
-    element.target.setAttribute("disabled", true);
+btnStop.addEventListener('click', element => {
+    btnStop.setAttribute("disabled", true);
     btnStart.removeAttribute("disabled");
 
-    clearInterval(intervalid);
-    })
+    clearInterval(intervalId);
+});
