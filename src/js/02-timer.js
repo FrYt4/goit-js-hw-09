f1313f5dec936e882c060f723ea6cf2108741d08
FrYt4 <adrian.fryczkie@yahoo.com>
2024-02-09 19:47:11 +0100

    hwsecond

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,64 +4,67 @@ import 'flatpickr/dist/flatpickr.min.css';
 // --------------------------SET VARS---------------------------------------------//
 const ref ={
 timerWrapper: document.querySelector('div.timer'),
-timerFields: document.querySelectorAll('div.field'),
 timerValues: document.querySelectorAll('span.value'),
-dateInput: document.querySelector(`input [type="text"]`),
+dateInput: document.querySelector(`input[type="text"]`),
 startButton: document.querySelector('button[data-start]')
 };
-const {timerWrapper , timerFields , timerValues , dateInput , startButton} = ref;
+
 
 // --------------------------SET STYLE---------------------------------------------//
+const { timerWrapper, timerValues, dateInput, startButton } = ref;
+
 timerWrapper.style.display = 'flex';
 timerWrapper.style.gap = '10px';
 timerWrapper.style.marginTop = '25px';
 
-timerFields.forEach(label => {
-    label.style.display = 'flex';
-    label.style.flexDirection = 'column';
-    label.style.fontSize = '20px';
-    label.style.alignItems = 'center';
-    label.style.fontFamily = 'monospace'
+document.querySelectorAll('div.field').forEach(label => {
+  label.style.display = 'flex';
+  label.style.flexDirection = 'column';
+  label.style.fontSize = '20px';
+  label.style.alignItems = 'center';
+  label.style.fontFamily = 'monospace';
 });
 
 timerValues.forEach(value => {
-    value.style.fontSize = '28px';
-
+  value.style.fontSize = '28px';
 });
-
 // --------------------------IMPLEMENT FUNCTIONS---------------------------------------------//
 function convertMs(ms) {
     // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
 
-function timerSets({days, hours, minutes, second}){
+        return {
+            days: Math.floor(ms / day),
+            hours: Math.floor((ms % day) / hour),
+            minutes: Math.floor(((ms % day) % hour) / minute),
+            seconds: Math.floor((((ms % day) % hour) % minute) / second)
+          };
+        }
+
+function timerSets({days, hours, minutes, seconds}) {
 const setsRefs = {
     timerDays: document.querySelector('span[data-days]'),
-    timerHours:document.querySelector('span[data-hours]'),
-    timerMinutes:document.querySelector('span[data-minutes]'),
-    timerSeconds:document.querySelector('span[data-seconds]')
+    timerHours: document.querySelector('span[data-hours]'),
+    timerMinutes: document.querySelector('span[data-minutes]'),
+    timerSeconds: document.querySelector('span[data-seconds]')
 };
 const {timerDays, timerHours, timerMinutes, timerSeconds} = setsRefs;
+const totalSeconds = days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds;
 timerDays.textContent = `${days}`;
 timerHours.textContent = `${hours}`;
 timerMinutes.textContent = `${minutes}`;
 timerSeconds.textContent = `${seconds}`;
-};
+
+const countdown = setInterval(() => {
+    if(totalSeconds <= 0){
+    clearInterval(countdown);
+    return;
+}
+},1000);
+}
 
 
 // --------------------------RECEIVE AN OBJECT OF PICKR PARAMETERS---------------------------------------------//
@@ -72,29 +75,27 @@ const options = {
     minuteIncrement: 1,
     isActive: false,
     onClose(selectedDates) {
-      if(new Date() - selectedDates[0] > 0){
-        alert('Please choose a date in the future');
-        startButton.disabled = !this.isActive;
-        return;
+        if (new Date() - selectedDates[0] > 0) {
+          alert('Please choose a date in the future');
+          startButton.disabled = !this.isActive;
+          return;
       }
       startButton.disabled = this.isActive;
-      startButton.addEventListener('click' , evt =>{
+      startButton.addEventListener('click' , (evt) => {
         if(this.isActive){
             return;
         };
         this.isActive = true;
         startButton.disabled = !this.isActive;
-        const convert = convertMs.bind(options);
         const timerId = setInterval(() => {
             if(selectedDates[0] - new Date() <=0){
                 clearInterval(timerId);
                 return;
             };
-            const face = timerSets.bind(options);
-            face(convert(selectedDates[0] - new Date()));
+            timerSets(convertMs(selectedDates[0] - new Date()));
         }, 1000);
-      });
-    },
+    });
+},
 };
 // --------------------------OTHER---------------------------------------------//
 
